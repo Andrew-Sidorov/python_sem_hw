02d83573a31274de0b3ce75a722d58b4f8c2eb0c
Andrew <sidorov_a_e@mail.ru>
2023-03-14 22:13:56 +0300

    дз.Задача34_и_36

--- Homework/36.py
+++ Homework/36.py
@@ -5,15 +5,13 @@
 # Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например,
 # у операции умножения.
 
-def print_operation_table(operation, num_rows=9, num_columns=9):
+def print_operation_table(operation, num_rows=6, num_columns=6):
 
     for x in range(1, num_rows + 1):
         nums = []
 
-
  for y in range(1, num_columns + 1):
     num = operation(x, y)
     nums.append(num)
-print('\t\t'.join([str(x) for x in nums]))
+print()
 
-Что тут надо поправить?
